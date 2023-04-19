cc225a36c7e5b10c001be6363b599b19cc7d6ae6
santos-eric <esantosdev77@gmail.com>
2023-04-19 04:45:24 -0600

    test

---
+++ c/address.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "Hi!";
+    printf("%s/n", s)
+}
+
+// type definition, char * (for data address in memory), name of variable always points to the first char
+
+typedef char *string
